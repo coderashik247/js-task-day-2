33090441adec7c78eef09e95b852b3dc6141d733
Ashik <ashikurashik20999@gmail.com>
2024-09-01 06:06:49 +0600

    task_5 added

---
+++ task_5.js
@@ -0,0 +1,16 @@
+var num1 = 8;
+var num2 = 9;
+
+var result;
+
+// if(num1 > num2){
+//     result = num1 * 2;
+//     console.log(result);
+// }
+// else{
+//     result = num1 + num2;
+//     console.log(result);
+// }
+
+result = (num1 > num2)? num1*2 : num1 + num2;
+console.log(result);
